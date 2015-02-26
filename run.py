713d33dc73ffc4642f42915c94218ce5c9542f8a
Battleroid <casweed@gmail.com>
2015-02-26 15:11:45 -0500

    added debug toolbar

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from nestegg import app
+from flask_debugtoolbar import DebugToolbarExtension
 
 if __name__ == '__main__':
     app.config.from_object('config.Development')
+    toolbar = DebugToolbarExtension(app)
     app.run(host='0.0.0.0')
